5dbe7029ceb10681d63672eb99f28cfa0e42f9dd
David J. Hammaker <davidjosh93@gmail.com>
2018-12-17 13:42:01 -0500

    Configure database
    Database is configured using flask-sqlalchemy extension. Database URI is
    configured to an environment variable called "IC_DATABASE". The
    SQLAlchemy object is initiated as an instance called 'db'.

--- item_catalog.py
+++ item_catalog.py
@@ -1,10 +1,14 @@
 import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from forms import NewItemForm
 
 app = Flask(__name__)
 
 app.secret_key = os.environ.get('IC_SECRET')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('IC_DATABASE')
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
