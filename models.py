bd9c09bc88365f4750592d74bbc0923844c39013
David J. Hammaker <davidjosh93@gmail.com>
2018-12-17 13:48:59 -0500

    Create Item, User, and OAuth models in 'models.py'
    Item model has columns 'id' (Integer), 'name' (String), 'sport'
    (String), 'category' (String), 'description' (String), 'date' (String).
    Item model also has a customized '__repr__'. User model has columns 'id'
    (Integer), 'username' (String), 'email' (String), 'name' (String), and a
    back-reference for the Item model, called 'user'. The User model
    inherits from 'UserMixin'. The OAuth model has columns
    'provider_user_id' (String), 'user_id' (Integer with a 'ForeignKey' of
    'User.id'), and 'user' (reference column to User model).

---
+++ models.py
@@ -0,0 +1,29 @@
+from flask_dance.consumer.backend.sqla import OAuthConsumerMixin
+from flask_login import UserMixin
+from item_catalog import db
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+    sport = db.Column(db.String(256), nullable=False)
+    category = db.Column(db.String(256), nullable=False)
+    description = db.Column(db.String(1024), nullable=False)
+    date = db.Column(db.String(256), nullable=False)
+
+    def __repr__(self):
+        return f"Item('{self.name}', '{self.sport}', '{self.category}', '{self.date}')"
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), nullable=False, unique=True)
+    email = db.Column(db.String(256), unique=True)
+    name = db.Column(db.String(256))
+    items = db.relationship('Item', backref='user', lazy=True)
+
+
+class OAuth(db.Model, OAuthConsumerMixin):
+    provider_user_id = db.Column(db.String(256), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = db.relationship(User)
