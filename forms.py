680a97266e2f6e27f376b58c2489a27a0e7244af
David J. Hammaker <davidjosh93@gmail.com>
2018-12-17 12:41:42 -0500

    Fix bad categories in 'NewItemForm'
    Categories in each SelectField need to be a list of name-value pairs.

--- forms.py
+++ forms.py
@@ -5,21 +5,21 @@ from wtforms.validators import DataRequired, Length
 
 class NewItemForm(FlaskForm):
     name = StringField('Item Name', validators=[DataRequired(), Length(max=64)])
-    sport = SelectField('Sport', choices=['Baseball',
-                                          'Basketball',
-                                          'Bowling',
-                                          'Boxing',
-                                          'Football',
-                                          'Golf',
-                                          'Hockey',
-                                          'Soccer',
-                                          'Tennis',
-                                          'Other'],
+    sport = SelectField('Sport', choices=[('Baseball', 'Baseball'),
+                                          ('Basketball', 'Basketball'),
+                                          ('Bowling', 'Bowling'),
+                                          ('Boxing', 'Boxing'),
+                                          ('Football', 'Football'),
+                                          ('Golf', 'Golf'),
+                                          ('Hockey', 'Hockey'),
+                                          ('Soccer', 'Soccer'),
+                                          ('Tennis', 'Tennis'),
+                                          ('Other', 'Other')],
                         validators=[DataRequired()])
-    category = SelectField('Category', choices=['Accessories',
-                                                'Apparel',
-                                                'Equipment',
-                                                'Fan Gear'],
+    category = SelectField('Category', choices=[('Accessories', 'Accessories'),
+                                                ('Apparel', 'Apparel'),
+                                                ('Equipment', 'Equipment'),
+                                                ('Fan Gear', 'Fan Gear')],
                            validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired(), Length(max=1024)])
     date = StringField('Date', validators=[DataRequired()])
