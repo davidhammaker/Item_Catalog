9bcb771abc8383dd3b467f5f8971e75aae155b9f
David J. Hammaker <davidjosh93@gmail.com>
2018-12-17 12:45:49 -0500

    Create 'new_item' view function with template
    The new 'new_item' view function recieves 'GET' and 'POST' requests. The
    view function renders an instance of 'NewItemForm' from 'forms.py'. The
    form is rendered in 'new_item.html', which requires a secret key (set to
    an environment variable called 'IC_SECRET'). The template renders each
    form field label, followed by respective form fields, and a submit
    button.

--- item_catalog.py
+++ item_catalog.py
@@ -1,11 +1,21 @@
+import os
 from flask import Flask, render_template
+from forms import NewItemForm
 
 app = Flask(__name__)
+
+app.secret_key = os.environ.get('IC_SECRET')
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/new_item', methods=['GET', 'POST'])
+def new_item():
+    form = NewItemForm()
+    return render_template('new_item.html', form=form)
 
 
 if __name__ == '__main__':
