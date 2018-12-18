f5b5175c955243e763a8feb47e154af1cc724530
David J. Hammaker <davidjosh93@gmail.com>
2018-12-18 12:10:01 -0500

    Add 'edit_item' view function and template
    Items may now be modified through the 'edit_item' view function, which
    renders the 'edit_item' template. The template contains the NewItemForm
     which is pre-filled with existing item information. If the item name or
     sport change, the function checks whether an item of the same name
     already exists for the given sport. If the function finds an item of
     the same name, the user is redirected to the same 'edit_item' view with
     a flash message informing the user about the conflict. If no conflicts
     exist, the item is updated in the database.

--- item_catalog/views.py
+++ item_catalog/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, abort
+from flask import render_template, redirect, url_for, flash, abort, request
 from item_catalog import app, db
 from item_catalog.forms import NewItemForm
 from item_catalog.models import Item, User
@@ -40,3 +40,33 @@ def item(item_name):
     if not item:
         abort(404)
     return render_template('item.html', item=item)
+
+
+@app.route('/item/<string:item_name>/edit', methods=['GET', 'POST'])
+def edit_item(item_name):
+    item = Item.query.filter_by(name=item_name).first()
+    if not item:
+        abort(404)
+    form = NewItemForm()
+    if form.validate_on_submit():
+        if form.name.data != item.name or form.sport.data != item.sport:
+            query = Item.query.filter_by(name=form.name.data, sport=form.sport.data).first()
+            if query:
+                flash('This sport already has an item with that name.', 'bad')
+                return redirect(url_for('edit_item', item_name=item_name))
+        else:
+            item.name = form.name.data
+            item.sport = form.sport.data
+            item.category = form.category.data
+            item.description = form.description.data
+            item.private = form.private.data
+            db.session.commit()
+            flash(f'"{item.name}" has been updated!', 'good')
+            return redirect(url_for('item', item_name=item_name))
+    elif request.method == 'GET':
+        form.name.data = item.name
+        form.sport.data = item.sport
+        form.category.data = item.category
+        form.description.data = item.description
+        form.private.data = item.private
+    return render_template('edit_item.html', item=item, form=form)
