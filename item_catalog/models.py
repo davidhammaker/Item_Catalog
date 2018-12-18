3cf6a1d95d2f51bd29d23bbc16f44cbf0cc50b55
David J. Hammaker <davidjosh93@gmail.com>
2018-12-17 23:03:32 -0500

    Fix Item model to include user ID foreign key

--- item_catalog/models.py
+++ item_catalog/models.py
@@ -11,6 +11,7 @@ class Item(db.Model):
     category = db.Column(db.String(256), nullable=False)
     description = db.Column(db.String(1024), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Item('{self.name}', '{self.sport}', '{self.category}', '{self.date}')"
