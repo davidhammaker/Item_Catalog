0e6529e4a31392933a29f710454736aa010160df
David J. Hammaker <davidjosh93@gmail.com>
2018-12-18 17:44:03 -0500

    Update 'sport' view to abort bad URLs
    If a URL contains a non-existent sport, the view will abort with a 404.

--- item_catalog/views.py
+++ item_catalog/views.py
@@ -22,6 +22,18 @@ def all_items():
 
 @app.route('/sport/<string:sport>')
 def sport(sport):
+    sports = ['Baseball',
+              'Basketball',
+              'Bowling',
+              'Boxing',
+              'Football',
+              'Golf',
+              'Hockey',
+              'Soccer',
+              'Tennis',
+              'Other']
+    if sport not in sports:
+        abort(404)
     page = request.args.get('page', 1, type=int)
     items = Item.query.filter_by(sport=sport).order_by(Item.name).paginate(page=page, per_page=10)
     return render_template('all.html', items=items)
