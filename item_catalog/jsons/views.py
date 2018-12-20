820e539553d5d69146e7b2d0d7a038b52505a4c3
David J. Hammaker <davidjosh93@gmail.com>
2018-12-20 10:52:28 -0500

    Add 'sport_json' view function
    Renders all public items for a given sport in JSON format.

--- item_catalog/jsons/views.py
+++ item_catalog/jsons/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort
 from item_catalog.models import Item
 from item_catalog.jsons.utils import json_prep
 
@@ -15,8 +15,32 @@ def all_items_json():
 @jsons.route('/recent/JSON')
 def recent_json():
     """Render a JSON page with recent items."""
-    query = Item.query.filter_by(private=False).order_by(Item.date.desc()).all()
+    query = Item.query.filter_by(private=False).order_by(Item.date.desc())\
+        .all()
     items = []
     for i in range(10):
         items.append(query[i])
+    return jsonify(json_prep(items))
+
+
+@jsons.route('/sport/<string:sport>/JSON')
+def sport_json(sport):
+    """Render a JSON page with all public items for a given sport.
+
+    Keyword arguments:
+    sport -- the sport by which items are filtered
+    """
+    sports = ['Baseball',
+              'Basketball',
+              'Bowling',
+              'Boxing',
+              'Football',
+              'Golf',
+              'Hockey',
+              'Soccer',
+              'Tennis',
+              'Other']
+    if sport not in sports:
+        abort(404)
+    items = Item.query.filter_by(sport=sport).order_by(Item.name).all()
     return jsonify(json_prep(items))
