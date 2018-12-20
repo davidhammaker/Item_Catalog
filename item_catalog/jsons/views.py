1acdef6fffcd5589f7f1255aa679064499c36054
David J. Hammaker <davidjosh93@gmail.com>
2018-12-20 10:49:04 -0500

    Add 'recent_json' view function
    Render 10 most recent public items in JSON format. Uses 'json_prep' 
    function to convert Item model type to dictionary type before using 
    jsonify.

--- item_catalog/jsons/views.py
+++ item_catalog/jsons/views.py
@@ -10,3 +10,13 @@ def all_items_json():
     """Render a JSON page with all public items."""
     items = Item.query.order_by(Item.name).filter_by(private=False).all()
     return jsonify(json_prep(items))
+
+
+@jsons.route('/recent/JSON')
+def recent_json():
+    """Render a JSON page with recent items."""
+    query = Item.query.filter_by(private=False).order_by(Item.date.desc()).all()
+    items = []
+    for i in range(10):
+        items.append(query[i])
+    return jsonify(json_prep(items))
