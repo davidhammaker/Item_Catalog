b7cde3a00be2709d529fdf077368477ed186ff3d
David J. Hammaker <davidjosh93@gmail.com>
2018-12-20 11:49:08 -0500

    Update 'json_prep'
    The 'json_prep' function now accepts single items in addition to lists 
    of multiple items. The function contains a conditional to check whether 
    'items' is a list before prepping 'items' as a list of dictionaries. If 
    'items' is not a list, the function preps 'items' as a single 
    dictionary.

--- item_catalog/jsons/utils.py
+++ item_catalog/jsons/utils.py
@@ -1,14 +1,26 @@
 def json_prep(items):
-    prepped = []
-    for item in items:
-        prepped_item = {
-            'id': item.id,
-            'name': item.name,
-            'sport': item.sport,
-            'category': item.category,
-            'description': item.description,
-            'date': item.date,
-            'user': item.user.username
+    if type(items) == type([]):
+        prepped = []
+        for item in items:
+            prepped_item = {
+                'id': item.id,
+                'name': item.name,
+                'sport': item.sport,
+                'category': item.category,
+                'description': item.description,
+                'date': item.date,
+                'user': item.user.username
+            }
+            prepped.append(prepped_item)
+        return {'items': prepped}
+    else:
+        prepped = {
+            'id': items.id,
+            'name': items.name,
+            'sport': items.sport,
+            'category': items.category,
+            'description': items.description,
+            'date': items.date,
+            'user': items.user.username
         }
-        prepped.append(prepped_item)
-    return {'items': prepped}
+        return {'item': prepped}
